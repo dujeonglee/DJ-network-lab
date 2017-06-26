b3aaa24f75df37c6cac38f97e345b29e48c023f2
Dujeong Lee <dujeong.lee82@gmail.com>
2017-06-26 20:12:37 +0900

    Newlines are added.

--- arp_spoofing/ARPSpoof.h
+++ arp_spoofing/ARPSpoof.h
@@ -1,9 +1,31 @@
-#ifndef _ARP_SPOOF_#define _ARP_SPOOF_
-#include <utility>#include <vector>
-enum IPVERSION : unsigned char{ IPV4 = 0, IPV6};
-class ARPSpoof{private:/*PRIVATE STATIC VARIABLE*/    static ARPSpoof* g_Instance;public:/*PUBLIC STATIC FUNCTION*/    static ARPSpoof* Instance();private:/*PRIVATE STATIC FUNCTION*/
-private:/*PRIVATE CLASS VARIABLE*/    std::vector< int > m_RxSockets;    int m_TxSocket;    unsigned char m_TxBuffer[1024];    unsigned char m_RxBuffer[1024];
-private:/*PRIVATE CLASS FUNCTION*/    bool HWAddress(const char* const ifname, unsigned char* const hw_address);    ARPSpoof();    ~ARPSpoof();
-public:    void DoARPSpoof(const char *ifname, const char* filename);
+#ifndef _ARP_SPOOF_
+#define _ARP_SPOOF_
+
+#include <utility>
+#include <vector>
+
+enum IPVERSION : unsigned char
+{
+    IPV4 = 0,
+    IPV6
+};
+
+class ARPSpoof{
+private:/*PRIVATE STATIC VARIABLE*/
+    static ARPSpoof* g_Instance;
+public:/*PUBLIC STATIC FUNCTION*/
+    static ARPSpoof* Instance();
+private:/*PRIVATE STATIC FUNCTION*/
+    static bool HWAddress(const char* const ifname, unsigned char* const hw_address);
+private:/*PRIVATE CLASS VARIABLE*/
+    std::vector< int > m_RxSockets;
+    int m_TxSocket;
+    unsigned char m_TxBuffer[1024];
+    unsigned char m_RxBuffer[1024];
+private:/*PRIVATE CLASS FUNCTION*/
+    ARPSpoof();
+    ~ARPSpoof();
+public:
+    void DoARPSpoof(const char *ifname, const char* filename);
 };
 #endif /*_ARP_SPOOF_WIRED_*/
--- arp_spoofing/main.cpp
+++ arp_spoofing/main.cpp
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "arp_spoof.h"
+#include "ARPSpoof.h"
 
 int main(int argc, char* argv[]){
-    arp_spoof::instance()->do_arp_spoof((argc > 1?argv[1]:NULL), (argc > 2?argv[2]:NULL));
+    ARPSpoof::Instance()->DoARPSpoof((argc > 1?argv[1]:NULL), (argc > 2?argv[2]:NULL));
     return 0;
 }
