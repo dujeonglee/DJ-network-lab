07c5741fdbb1259f1b12974dea5f9317e766e686
Dujeong Lee <dujeong.lee82@gmail.com>
2017-07-06 14:13:08 +0900

    Update and rename BroadcastForward.h to BroadMulticastForward.h

--- BroadcastForward/BroadMulticastForward.h
+++ BroadcastForward/BroadMulticastForward.h
@@ -2,8 +2,6 @@
 #define _BROADCAST_FORWARD_
 
 #include <cstdint>
-#include <utility>
-#include <vector>
 #include <string>
 
 #include "md5.h"
@@ -16,30 +14,47 @@ struct MD5
     md5_byte_t stream[16];
 };
 
-class BroadcastForward
+struct HWAddressType
+{
+    uint8_t address[6];
+};
+
+enum NetworkInterfaceType
+{
+    WIRELESS = 0,
+    WIRED,
+    MAX_NETWORK_INTERFACES
+};
+
+class BroadMulticastForward
 {
 private:/*PRIVATE STATIC VARIABLE*/
-    static BroadcastForward* g_Instance;
+    static BroadMulticastForward* g_Instance;
 public:/*PUBLIC STATIC FUNCTION*/
-    static BroadcastForward* Instance();
+    static BroadMulticastForward* Instance();
 private:/*PRIVATE STATIC FUNCTION*/
     static bool HWAddress(const char* const ifname, unsigned char* const hw_address);
     static MD5 MessageDigest(const void* const data, const uint32_t length);
     static std::string MessageDigestStr(const void* const data, const uint32_t length);
 private:/*PRIVATE CLASS VARIABLE*/
-    int m_RxSocket;
-    int m_TxSocket;
-    unsigned char HWAddr[6];
+    std::atomic<bool> m_Running;
+    int m_Sockets[MAX_NETWORK_INTERFACES];
+    std::string m_InterfaceNames[MAX_NETWORK_INTERFACES];
+    HWAddressType m_InterfaceHWAddresses[MAX_NETWORK_INTERFACES];
     unsigned char m_TxBuffer[1024];
     unsigned char m_RxBuffer[1024];
-    std::string m_IfName;
+
     AVLTree<MD5, uint8_t> m_MD5;
     ThreadPool<2, 1> m_ThreadPool;
     SingleShotTimer<2, 1> m_Timer;
+
 private:/*PRIVATE CLASS FUNCTION*/
-    BroadcastForward();
-    ~BroadcastForward();
+    BroadMulticastForward();
+    ~BroadMulticastForward();
+    void Forward();
 public:
-    void Forward(const std::string ifname);
+    void SetNetworkInterface(const NetworkInterfaceType, const std::string);
+    bool Start();
+    void Stop();
 };
 #endif /*_ARP_SPOOF_WIRED_*/
