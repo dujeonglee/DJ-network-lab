4fe277a82d7f30eb1a07184f4db439244ebefd7a
Dujeong Lee <dujeong.lee82@gmail.com>
2017-07-06 14:12:11 +0900

    Update main.cpp

--- BroadcastForward/main.cpp
+++ BroadcastForward/main.cpp
@@ -1,10 +1,16 @@
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "BroadcastForward.h"
+#include "BroadMulticastForward.h"
 
 int main(int argc, char* argv[]){
-    BroadcastForward::Instance()->Forward((argc > 1?std::string(argv[1]):std::string("")));
-    while(1);
+    BroadMulticastForward::Instance()->SetNetworkInterface(WIRELESS, "enp0s31f6");
+    BroadMulticastForward::Instance()->SetNetworkInterface(WIRED, "docker0");
+    BroadMulticastForward::Instance()->Start();
+    while(1)
+    {
+        sleep(1);
+    }
     return 0;
 }
